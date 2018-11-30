09db9aeb5a1de8ef5877c7ff8a68f0698a85d972
Pietro Alovisi <pietro.alovisi@outlook.it>
2018-11-30 22:44:43 +0100

    added file

---
+++ ass1/bayes_linear.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+class BayesLinearRegression:
+	def __init__(self, prior_mean,prior_cov, precision):
+		self.prior_mean = prior_mean
+		self.prior_cov = prior_cov
+
+		self.posterior_mean = prior_mean
+		self.posterior_cov = prior_cov
+
+		self.precision = precision
+
+	def sample_posterior():
+		pass
+
+	def put_data(x,y):
+		self.posterior_cov = np.linalg.inv(np.linalg.inv(self.prior_cov) + self.precision* np.outer(x,x))
+		self.posterior_mean = np.dot(self.posterior_cov, ( np.dot(np.linalg.inv(self.prior_cov),self.prior_mean)) + (self.precision*x*y) )
+		
+		self.prior_cov = self.posterior_cov
+		self.prior_mean = self.posterior_mean
